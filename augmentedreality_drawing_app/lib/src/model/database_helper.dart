38a32031f6d7a28017c71fa2ade4fd6cdf2d840c
Myndaaa <myshaaa31ns@gmail.com>
2024-09-07 12:26:32 +0800

    .

--- augmentedreality_drawing_app/lib/src/model/database_helper.dart
+++ augmentedreality_drawing_app/lib/src/model/database_helper.dart
@@ -1,5 +1,3 @@
-// lib/services/database_helper.dart
-
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:augmentedreality_drawing_app/src/model/svgfile_datamodel.dart';
